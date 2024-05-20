7d9c6afaf39f8308dc67f263ff885747c4aa5324
ChuanWei <b09502053@ntu.edu.tw>
2024-05-20 23:42:36 +0800

    Rename project from PicktureBook to PictureBook

--- PictureBook/urls.py
+++ PictureBook/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for PicktureBook project.
+URL configuration for PictureBook project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
